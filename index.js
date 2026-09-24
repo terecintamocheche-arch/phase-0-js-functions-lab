a83046feead5f4721192568394583042ed40e479
Terecinta Mocheche <terecintamocheche@gmail.com>
2026-09-24 20:44:25 +0300

    Convert to upper case

--- index.js
+++ index.js
@@ -12,15 +12,3 @@ function calculateTax(amount) {
 function convertToUpperCase(text) {
   return text.toUpperCase();
 }
-
-function findMaximum(num1, num2) {
-  return Math.max(num1, num2);
-}
-
-function isPalindrome(word) {
-  return word === word.split("").reverse().join("");
-}
-
-function calculateDiscountedPrice(originalPrice, discountPercentage) {
-  return originalPrice - (originalPrice * discountPercentage / 100);
-}
