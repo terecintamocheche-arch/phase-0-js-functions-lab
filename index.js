e3bfa6c2543ab7a1e0ed612229af6a2a53ea6373
Terecinta Mocheche <terecintamocheche@gmail.com>
2026-09-24 20:43:30 +0300

    calculate tax created

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function calculateTax(amount) {
+  return amount * 0.10;
+}
+
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+  return Math.max(num1, num2);
+}
+
+function isPalindrome(word) {
+  return word === word.split("").reverse().join("");
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * discountPercentage / 100);
+}
